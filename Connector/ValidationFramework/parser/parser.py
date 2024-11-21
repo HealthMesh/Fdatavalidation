f091b88cdb660c9dbac5d6b2f4b030ca933fb69b
aacraf <aacraaf@gmail.com>
2024-11-21 22:10:19 +0100

    exp + tests with odrl-pap

--- Connector/ValidationFramework/parser/parser.py
+++ Connector/ValidationFramework/parser/parser.py
@@ -23,21 +23,16 @@ prov = Namespace("http://www.w3.org/ns/prov#")
 dqv = Namespace("http://www.w3.org/ns/dqv#")
 
 
-# Utility functions
 def generate_unique_uri(base_uri):
-    """Generate a unique URI based on the base URI."""
     unique_identifier = str(uuid.uuid4())
     return URIRef(f"{base_uri}{unique_identifier}")
 
 def load_jsonld_data(file_path):
-    """Load and parse JSON-LD data from a file."""
     with open(file_path, 'r') as f:
         return json.loads(f.read())
 
 
-# Decorator to measure the time taken by a function and store policy names
 def timeit(func):
-    """Decorator to measure time taken by a function."""
     def wrapper(self, policy, *args, **kwargs):
         start_time = time.time()
         result = func(self, policy, *args, **kwargs)
@@ -55,18 +50,19 @@ def timeit(func):
 # Policy Checker Class
 
 class PolicyChecker(Graph):
-    """Class representing the policy checker that validates policies."""
+    """ Create Policy Checker """
 
-    def __init__(self, p, p_type, data_format, *args, **kwargs):
+    def __init__(self, p, dp, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.p = p
-        self.p_type = p_type.split("/")[-1]
         self.bind("ab", "http://www.semanticweb.org/acraf/ontologies/2024/healthmesh/abox#")
         self.bind("tb", "http://www.semanticweb.org/acraf/ontologies/2024/healthmesh/tbox#")
         self.URI = generate_unique_uri(abox)
+
         self.add((self.URI, RDF.type, tbox.PolicyChecker))
-        self.add((self.URI, tbox.validates, p))
-        self.add((self.URI, tbox.hasType, data_format))
+        self.add((self.URI, tbox.accordingTo, p))
+        self.add((self.URI, tbox.validates, abox[dp]))
+        # self.p_type = p_type.split("/")[-1]
 
     def get_URI(self):
         return self.URI
@@ -80,39 +76,33 @@ class PolicyChecker(Graph):
 
 # Policy Parsing Class
 
+
 class DCParser:
-    """
-    Parse Policies of Data Contracts to Policy Checkers
-    """
 
     def __init__(self, dp, graph):
         self.dp = dp
         self.g = graph
         self.attr_mappings = None
 
-    def validate_graph(self) -> bool:
-        """
-        Validate the policies grammar is compliant with the grammar defined
-        :return: conformance/non-conformance
-        """
+    def _validate_graph(self) -> bool:
+
+        from pyshacl import validate
         shapes = Graph().parse(os.path.join(base_dir, 'policy_grammar.json'), format="turtle")
         conforms, report_graph, report_text = validate(self.g, shacl_graph=shapes)
+        # return boolean
         return conforms
 
-    def read_contracts(self):
-        """
-        Get the policies associated with a data product
-        :return: list of policies and mappings
-        """
+    def _read_contracts(self):
+
         contracts = self.g.objects(subject=abox[self.dp], predicate=tbox.hasDC)
-        policies_list, mappings_dict = [], {}
-
+        policies_list = []
+        mappings_dict = {}
         for contract in contracts:
-            # Handle policies
+            # handle policies
             policies = self.g.objects(subject=contract, predicate=tbox.hasPolicy)
-            policies_list.extend(policies)
-
-            # Handle attribute mappings
+            for policy in policies:
+                policies_list.append(policy)
+            # handle mappings
             mappings = self.g.objects(subject=contract, predicate=tbox.hasMapping)
             for mapping in mappings:
                 mfrom = self.g.value(subject=mapping, predicate=tbox.mfrom)
@@ -122,106 +112,112 @@ class DCParser:
         self.attr_mappings = mappings_dict
         return policies_list, mappings_dict
 
-    def execute_rule(self, rule_path, pc, mappings):
-        """Execute SPARQL rules for policy validation."""
+    def executRule(self, rule_path, pc, mappings):
+
         for sparqlrule in os.listdir(rule_path):
             with open(os.path.join(rule_path, sparqlrule), 'r') as file:
                 rule = file.read()
+
                 for key, value in mappings.items():
                     rule = rule.replace(f"<{{{key}}}>", f"<{value}>")
+
                 try:
                     results = self.g.query(rule)
+
                     result_graph = Graph()
+
                     for triple in results:
                         result_graph.add(triple)
+
                     pc += result_graph
                 except Exception as e:
                     print("Parsing Error: ", e)
+
         return pc
 
     def get_last_op(self, pc):
-        """Retrieve the last operation of a policy checker."""
+
         last_op = pc.value(subject=pc.get_URI(), predicate=tbox.nextStep)
-        while pc.value(subject=last_op, predicate=tbox.nextStep):
+        while last_op:
+            if not pc.value(subject=last_op, predicate=tbox.nextStep):
+                break
             last_op = pc.value(subject=last_op, predicate=tbox.nextStep)
         return last_op
 
-    def init_op(self, policy, pc):
-        """Initialize operations for a policy."""
+    def _initOP(self, policy, pc):
+        """
+        :param IR:
+        :param policy:
+        :return:
+        """
+
         initOPrules = os.path.join(base_dir, "rules/initOP")
         mappings = {
             "dp": abox[self.dp],
             "pc": pc.get_URI(),
             "op_uri": generate_unique_uri(abox),
         }
-        pc = self.execute_rule(initOPrules, pc, mappings)
+
+        pc = self.executRule(initOPrules, pc, mappings)
+
         return self.get_last_op(pc), pc
 
-    def handle_attributes(self, pc):
-        """Update attributes in the policy checker according to mappings."""
+    def _handle_attributes(self, pc):
         operation = pc.get_URI()
         while operation:
-            attribute = pc.value(subject=operation, predicate=tbox.hasAttribute)
-            if attribute and attribute in self.attr_mappings:
-                pc.remove((operation, tbox.hasAttribute, None))
-                pc.add((operation, tbox.hasAttribute, self.attr_mappings[attribute]))
+            if pc.value(subject=operation, predicate=tbox.hasInput):
+                attributes = pc.objects(subject=operation, predicate=tbox.hasInput)
+                for attribute in attributes:
+                    if attribute in self.attr_mappings.keys():
+                        pc.remove((operation, tbox.hasInput, attribute))
+                        pc.add((operation, tbox.hasInput, self.attr_mappings[attribute]))
             operation = pc.value(subject=operation, predicate=tbox.nextStep)
         return pc
 
-    def handle_duties(self, pc, init_op):
-        """Handle duties in a policy checker."""
-        ops_rules_path = os.path.join(base_dir, "rules/OPS")
+    def _handle_policy_patterns(self, pc, initOP):
+
+        initOPrules = os.path.join(base_dir, "rules/OPS")
         mappings = {
             "dp": abox[self.dp],
             "pc": pc.get_URI(),
             "op_uri": generate_unique_uri(abox),
-            "last_op": init_op,
+            "last_op": initOP,
             "policy_uri": pc.get_policy(),
         }
-        pc = self.execute_rule(ops_rules_path, pc, mappings)
+        pc = self.executRule(initOPrules, pc, mappings)
+
         return self.get_last_op(pc), pc
 
-    def plot_times(self, filename='policy_times.png'):
-        """Plot the time taken to parse each policy."""
-        plt.bar(self.parse_policy.policy_names, self.parse_policy.times)
-        plt.title('Time taken to parse each policy')
-        plt.xlabel('Policy')
-        plt.ylabel('Time (seconds)')
-        plt.xticks(rotation=90)  # Rotate policy names for better readability
-        plt.tight_layout()  # Adjust layout to make room for labels
-        plt.savefig(filename)
-        plt.close()
+    def _parse_policy(self, policy):
 
-    @timeit
-    def parse_policy(self, policy):
-        """Parse an individual policy into an intermediate representation."""
-        p_type = self.g.value(subject=policy, predicate=RDF.type)
-        data_format = self.g.value(subject=abox[self.dp], predicate=tbox.hasDTT)
-        pc = PolicyChecker(policy, p_type, data_format)
+        pc = PolicyChecker(policy, self.dp)
 
-        # Add initial operations and handle duties
-        last_op, pc = self.init_op(policy, pc)
-        last_op, pc = self.handle_duties(pc, last_op)
-        pc = self.handle_attributes(pc)
+        last_op, pc = self._initOP(policy, pc)
 
-        # Add final report
+        last_op, pc = self._handle_policy_patterns(pc, last_op)
+        pc = self._handle_attributes(pc)
+
+        # Report
         report_uid = generate_unique_uri(abox)
         pc.add((last_op, tbox.nextStep, report_uid))
         pc.add((report_uid, RDF.type, tbox.Report))
-
+        # DUTY
         return pc
 
     def parse_contracts(self):
-        """Parse all policies associated with a data product."""
-        policies, mappings = self.read_contracts()
+
+        # validate policies
+        # if self._validate_graph() == True:
+        # get policies
+        policies, mappings = self._read_contracts()
 
         for policy in policies:
-            print("Parsing Policy... ", policy)
-            pc = self.parse_policy(policy)
-            self.g += pc
+            pc = self._parse_policy(policy)
+            self.g = self.g + pc
 
-        output_path = os.path.join(base_dir, "../../../FederatedComputationalGovernance/SemanticDataModel/sdm.ttl")
-        self.g.serialize(destination=output_path, format="turtle")
+        self.g.serialize(
+            destination=os.path.join(base_dir, "../../../FederatedComputationalGovernance/SemanticDataModel/sdm.ttl"),
+            format="turtle")
 
         return self.g
 
